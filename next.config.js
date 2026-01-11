807f147011ab9c1376858dd5989a4f2b7b1f24f1
Yoshi Kondo <yksanjo@gmail.com>
2026-01-11 11:48:02 -0500

    Fix Next.js config and update version

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  // Server Actions are enabled by default in Next.js 14
 }
 
 module.exports = nextConfig
